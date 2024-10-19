ea9b032dc2edbb99766cc7b6f4cb6d9a7c5d4361
tlmoody84 <156528348+tlmoody84@users.noreply.github.com>
2024-10-19 14:43:22 -0400

    added cors

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
+
 app.use(cors({
     origin: ['http://localhost:4001', 'https://breastfeeding-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
